190ef568b7a4cee1c17ebdb81fcde5f71da3f5e9
tomek-baczkowski <tomek.baczkowski@gmail.com>
2019-02-15 20:16:10 -0500

    clean files ready to work

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>PAGE TITLE</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Tomasz Baczkowski">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link href="CSS/normalize.css" rel="stylesheet" media="all">
+    <link href="CSS/style.css" rel="stylesheet" media="all">
+    <link href="CSS/print.css" rel="stylesheet" media="print">
+    <!--[if lt IE 9]><script src="js/html5shiv-printshiv.js" media="all"></script><![endif]-->
+    <script src="JS/script.js"></script>
+    <!-- favicon links-->
+    <!-- OpenGraph meta prop: og:title, og:image, og:description-->
+</head>
+<body>
+    <header role="banner">
+        <h1>Main Title</h1>
+        <nav role="navigation">
+
+        </nav>
+    </header>
+
+    <div class="wrap">
+        <main role="main">
+
+        </main>
+    </div>
+
+    <footer role="contentinfo">
+        <address>
+          <p>For further information, please contact <a href="mailto:admin@example.com">Max Mustermann</a>.</p>
+        </address>
+        <small>Copyright &copy; <time>2018</time></small>
+    </footer>
+
+</body>
+</html>
